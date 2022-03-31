dedb2bb0797375b2d36e1f33bfff17d888102785
Andrew Smith <andrew_c_smith@hotmail.com>
2022-03-31 16:13:45 +0100

    up to 7

---
+++ start_point/src/friends.py
@@ -0,0 +1,41 @@
+def get_name(person):
+    return person["name"] 
+
+def get_favourite_tv_show(tv_show):
+    return tv_show["favourites"]["tv_show"]
+    
+def likes_to_eat(person,food_to_find):
+    found_food = False
+    for food in person["favourites"]["snacks"]:
+        if food == food_to_find:
+            found_food = True
+    return found_food
+
+def add_friend(person, friend):
+    person["friends"].append(friend)
+
+
+def remove_friend(person,friend):
+    person["friends"].remove(friend)
+
+def total_money(money):
+    total_money = 0
+
+    for cash in money:
+        total_money += cash["monies"]
+        cash["monies"] = 0
+
+    return total_money
+        
+def lend_money(lender, lendee, amount):
+    lender["monies "]
+
+    person_lending = lender["monies"] 
+    person_getting = lendee["monies"] 
+
+    
+    
+    
+
+    
+    
